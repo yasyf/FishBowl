f8ab76fb95e55760ef4245bc4ee74b6db5ace0f9
Yasyf Mohamedali <yasyfm@gmail.com>
2015-03-17 20:02:35 -0400

    Initial Commit

---
+++ PersonLogTests/PersonLogTests.swift
@@ -0,0 +1,36 @@
+//
+//  PersonLogTests.swift
+//  PersonLogTests
+//
+//  Created by Yasyf Mohamedali on 2015-03-17.
+//  Copyright (c) 2015 Yasyf Mohamedali. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PersonLogTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
