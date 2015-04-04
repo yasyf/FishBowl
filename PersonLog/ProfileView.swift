367fe6a76d97df3c7695551fa4b8dc5acaac5510
Larry Zhang <lzhang124@gmail.com>
2015-04-04 03:33:12 -0400

    storyboard profile view

---
+++ PersonLog/ProfileView.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileView.swift
+//  PersonLog
+//
+//  Created by Larry Zhang on 4/4/15.
+//  Copyright (c) 2015 Yasyf Mohamedali. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    
+
+}
