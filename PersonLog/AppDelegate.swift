6627e2a7b96a286250838079504c55e822fa31b5
Larry Zhang <lzhang124@gmail.com>
2015-04-05 02:00:24 -0400

    error. trying to remerge

--- PersonLog/AppDelegate.swift
+++ PersonLog/AppDelegate.swift
@@ -44,12 +44,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         defaults.setValue("hxxp://foo.bar", forKey: "photo_url")
         defaults.setValue("LarryZ", forKey: "fb_id")
         
+        broadcaster.broadcast()
+        discoverer.discover()
+        
         return true
     }
 
-//    func application(application: UIApplication, openURL url: NSURL, sourceApplication: NSString?, annotation: AnyObject) -> Bool {
-//        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
-//    }
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: NSString?, annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
     
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
