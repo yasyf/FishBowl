21807d8114f4e9797d3de963c9dc772d1f704295
Yasyf Mohamedali <yasyfm@gmail.com>
2015-04-07 02:24:24 -0400

    Add logout, twitter, and snapchat helpers to Settings.

--- PersonLog/Settings.swift
+++ PersonLog/Settings.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Settings {
     let defaults = NSUserDefaults.standardUserDefaults()
-    let fields = ["f_name", "l_name", "phone", "photo_url", "fb_id", "twitter"]
+    let fields = ["f_name", "l_name", "phone", "photo_url", "fb_id", "twitter", "snapchat"]
     let lineColor = UIColor(red: 231.0/255.0, green: 145.0/255.0, blue: 42.0/255.0, alpha: 1.0).CGColor
     
     func _string(key: String) -> String? {
@@ -49,8 +49,20 @@ class Settings {
     func phone() -> String? {
         return _string("phone")
     }
-    func setphone(phone: String) {
+    func setPhone(phone: String) {
         defaults.setValue(phone, forKey: "phone")
+    }
+    func twitter() -> String? {
+        return _string("twitter")
+    }
+    func setTwitter(twitter: String) {
+        defaults.setValue(twitter, forKey: "twitter")
+    }
+    func snapchat() -> String? {
+        return _string("snapchat")
+    }
+    func setSnapchat(snapchat: String) {
+        defaults.setValue(snapchat, forKey: "snapchat")
     }
     func isLoggedIn() -> Bool {
         return facebookID() != nil
@@ -59,5 +71,8 @@ class Settings {
         for field in fields {
             defaults.setNilValueForKey(field)
         }
+    }
+    func logout() {
+        defaults.setNilValueForKey("fb_id")
     }
 }
